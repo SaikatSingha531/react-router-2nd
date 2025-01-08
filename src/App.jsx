5805739048385be8e05b7159b032ea6b421ce49d
Saikat Singha <saikatsingha531@gmail.com>
2025-01-09 01:33:19 +0530

    again commit for latest way of creating react router

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
 import Navbar from "./assets/Components/Navbar";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { createBrowserRouter,RouterProvider } from "react-router-dom";
 import Home from "./assets/Components/Home";
 import About from "./assets/Components/About";
 import Contact from "./assets/Components/Contact";
@@ -11,16 +11,38 @@ import Contact from "./assets/Components/Contact";
 
 function App() {
 
+
+  const router= createBrowserRouter([
+    {
+      path:"/",
+      element:<><Navbar/><Home/></>
+    },
+    {
+      path:"/about",
+      element:<><Navbar/><About/></>
+    },
+    {
+      path:"/contact",
+      element:<><Navbar/><Contact/></>
+    },
+  ])
+
+
   return (
     <>
-        <Router>
+    <RouterProvider router={router}/>      //this is the latest way of react touter 
+
+    {/* <Navbar/>   */}
+
+
+        {/* <Router>                    // this is the old way of using react router
             <Navbar/>
           <Routes>
             <Route path="/" element={<Home/>} />
             <Route path="/about" element={<About/>}/>
             <Route path="/contact" element={<Contact/>} />
           </Routes>
-        </Router>
+        </Router> */}
     </>
   );
 }
